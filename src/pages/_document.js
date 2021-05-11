aa5719a4285efc62dd71df01450011a8a981415f
Leonardo Piusi <leopidev@gmail.com>
2021-05-11 00:04:15 -0300

    fix: directory structure

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+
+          <link rel="shortcut icon" href="favicon.png" type="image/png/" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
